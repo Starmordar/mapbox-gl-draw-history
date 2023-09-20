6c0d77895312db607d61985abb5b57490ddde837
Roman Borovik <starmordar@gmail.com>
2023-09-20 12:52:55 +0300

    hotfix

---
+++ lib/setup.ts
@@ -0,0 +1,11 @@
+// import { IControl } from 'mapbox-gl';
+import type { IControlOptions } from './types';
+
+function setup(options: IControlOptions) {
+  //   console.log('window :>> ', window);
+  //   console.log(options);
+  //   console.log('123');
+  // return 'index 2'
+}
+
+export default setup;
