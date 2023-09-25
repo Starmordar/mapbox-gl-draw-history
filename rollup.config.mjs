b901d69f23fc208e379cc9011b13d899ce61ac05
Roman Borovik <starmordar@gmail.com>
2023-09-25 15:08:23 +0300

    Setup correct build steps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,20 +3,35 @@ import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 
-const developmentMode = process.env.ROLLUP_WATCH === 'true';
+const { BUILD, MINIFY } = process.env;
+const minified = MINIFY === 'true';
+const production = BUILD === 'production';
+
+function getBuildOutputFile(build, minified) {
+  switch (build) {
+    case 'production':
+      if (minified) return 'dist/mapbox-gl-draw-history.js';
+      return 'dist/mapbox-gl-draw-history-unminified.js';
+    default:
+      return 'dist/mapbox-gl-draw-history-dev.js';
+  }
+}
+const outputFile = getBuildOutputFile(BUILD, MINIFY);
 
 export default {
   input: './src/index.ts',
   output: {
     name: 'MapboxDrawHistory',
-    file: 'dist/mapbox-gl-draw-history.js',
+    file: outputFile,
     format: 'umd',
+    sourcemap: production ? true : 'inline',
+    indent: false,
   },
-  treeshake: !developmentMode,
+  treeshake: production,
   plugins: [
     typescript(),
     resolve({ moduleDirectories: ['node_modules'] }),
-    !developmentMode ? terser() : false,
-    babel({ extensions: ['.ts'], include: ['src/**/*.ts'], babelHelpers: 'bundled' }),
+    minified ? terser() : false,
+    babel({ extensions: ['.ts'], include: ['src/**/*.ts'] }),
   ],
 };
